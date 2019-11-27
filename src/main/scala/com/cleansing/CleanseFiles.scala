7a6c327ee5d32b14685e1665b791788855c4d288
Jd <jdsatpathy@gmail.com>
2019-11-27 14:04:09 +0530

    Change in filter for non empty check

--- src/main/scala/com/cleansing/CleanseFiles.scala
+++ src/main/scala/com/cleansing/CleanseFiles.scala
@@ -3,39 +3,16 @@ package com.cleansing
 import com.typesafe.config.Config
 import org.apache.spark.sql.{Column, DataFrame, SparkSession}
 import org.apache.spark.sql.functions._
-
-import scala.annotation.tailrec
-
 trait CleanseFiles {
-  /* VATUCC Validation*/
-
-  //V : Validity check of the data. e.g. card number contains only numbers,
-
-  //A : Accuracy check, e.g. revenue has to have value rounded to 3 decimal places
-
-  //T : Timeliness check of data e.g. if header date is sysdate-1
-
-  //U : Uniqueness check, e.g. identify duplicate data
-
-  //C : Consistency check, e.g. Child table primary keys are present in parent table
-
-  //C : Completeness check e.g.
-
   def cleanseFile(spark : SparkSession, envProp : Config, file : DataFrame) : DataFrame = {
-    /*orderId, orderDate, orderCustomerId, orderStatus
-    Col 1 and 3 if null then discard -- done
-    no duplicate rows --done
-    1st and 3rd column should be numbers --done
-    should have n columns in each row
-    */
     def discardNull(df : DataFrame, nonNullFields : Int*) : DataFrame = {
       if (nonNullFields == Nil || nonNullFields.isEmpty)
         df
       else {
         if (nonNullFields.tail != Nil) {
-          discardNull(df.filter(col(df.columns(nonNullFields.head)) =!= lit("null")), nonNullFields.tail : _*)
+          discardNull(df.filter(col(df.columns(nonNullFields.head)).isNotNull), nonNullFields.tail : _*)
         } else {
-          df.filter(col(df.columns(nonNullFields.head)) =!= lit("null"))
+          df.filter(col(df.columns(nonNullFields.head)).isNotNull)
         }
       }
     }
@@ -53,7 +30,7 @@ trait CleanseFiles {
 
 
     val distinctData = file.filter(r => r.toString().split(",").length == envProp.getInt("record.length")).distinct() // Ensures no duplicate
-    val nonNullCols = envProp.getString("non.null.columns").split(",").map(_.toInt) // Ensures no null passed
+    val nonNullCols : Seq[Int] = envProp.getString("non.null.columns").split(",").map(_.toInt) // Ensures no null passed
     val numberOnlyFields = envProp.getString("non.string.columns").split(",").map(_.toInt)
     val nonNullDf = discardNull(distinctData, nonNullCols : _*)
     numberOnly(nonNullDf, numberOnlyFields : _*)
--- src/main/scala/com/ingestion/IngestFiles.scala
+++ src/main/scala/com/ingestion/IngestFiles.scala
@@ -8,7 +8,7 @@ import org.apache.spark.SparkConf
 import org.apache.spark.sql.types.{IntegerType, StringType, StructField, StructType}
 import org.apache.spark.sql.{DataFrame, SparkSession}
 
-object IngestFiles extends LazyLogging with CleanseFiles with ValidateFiles with EnvPropertiesValidator{
+object IngestFiles extends LazyLogging with CleanseFiles with ValidateFiles with EnvPropertiesValidator {
 
   def main(args: Array[String]): Unit = {
     val envProp = ConfigFactory.load().getConfig(args(0))
@@ -16,27 +16,28 @@ object IngestFiles extends LazyLogging with CleanseFiles with ValidateFiles with
     val spark = sparkSessionWithHive(envProp, sparkConf)
     val fileName = envProp.getString("file.name")
     val filePath = envProp.getString("input.path")
-    try {
-      val cleansedFile = cleanseFile(spark, envProp, readFileToDataFrame(spark,filePath,envProp))
-      if (insertToStagingTable(spark, cleansedFile, envProp)) {
-        logger.info(s"$fileName file loaded successfully to staging table")
-      }
-      else {
-        logger.info(s"$fileName file loaded partially to staging table")
-        throw new Exception("Partial load exception")
-      }
-    } catch {
-      case e : Exception => logger.error(s"Exception occurred : ${e.getStackTrace}")
+    //    try {
+    val readFile = readFileToDataFrame(spark, filePath, envProp)
+    val cleansedFile = cleanseFile(spark, envProp, readFile)
+    if (insertToStagingTable(spark, cleansedFile, envProp)) {
+      logger.info(s"$fileName file loaded successfully to staging table")
     }
+    else {
+      logger.info(s"$fileName file loaded partially to staging table")
+      throw new Exception("Partial load exception")
+    }
+    //    } catch {
+    //      case e : Exception => logger.error(s"Exception occurred : ${e.getCause.getStackTrace}")
+    //    }
   }
 
-  def sparkConfCreator(envProp : Config) : SparkConf = {
+  def sparkConfCreator(envProp: Config): SparkConf = {
     new SparkConf()
       .setAppName(envProp.getString("app.name"))
       .setMaster(envProp.getString("app.master"))
   }
 
-  def sparkSessionDev(envProp: Config, sparkConf: SparkConf) : SparkSession = {
+  def sparkSessionDev(envProp: Config, sparkConf: SparkConf): SparkSession = {
     SparkSession
       .builder()
       .config(sparkConf)
@@ -44,23 +45,23 @@ object IngestFiles extends LazyLogging with CleanseFiles with ValidateFiles with
       .getOrCreate()
   }
 
-  def sparkSessionWithHive(envProp: Config, sparkConf: SparkConf) : SparkSession = {
+  def sparkSessionWithHive(envProp: Config, sparkConf: SparkConf): SparkSession = {
     SparkSession
       .builder()
-      .config("hive.metastore.uris",envProp.getString("hive.metastore"))
+      .config("hive.metastore.uris", envProp.getString("hive.metastore"))
       .config("spark.sql.warehouse.dir", envProp.getString("warehouse.dir"))
       .config(sparkConf)
       .enableHiveSupport()
       .getOrCreate()
   }
 
-  def sparkSessionSelect(sparkConf: SparkConf, envProp: Config, arg : String) : SparkSession = arg match {
+  def sparkSessionSelect(sparkConf: SparkConf, envProp: Config, arg: String): SparkSession = arg match {
     case "dev" => sparkSessionDev(envProp, sparkConf)
-    case "prod" => sparkSessionWithHive(envProp,sparkConf)
-    case _ => sparkSessionDev(envProp,sparkConf)
+    case "prod" => sparkSessionWithHive(envProp, sparkConf)
+    case _ => sparkSessionDev(envProp, sparkConf)
   }
 
-  def insertToStagingTable(spark: SparkSession, df : DataFrame, envProp : Config) : Boolean = {
+  def insertToStagingTable(spark: SparkSession, df: DataFrame, envProp: Config): Boolean = {
     val preLoadCount = df.count()
     df.write.mode("overwrite").insertInto(envProp.getString("table.name"))
     val postLoadCount = spark.read.table(envProp.getString("table.name")).count()
@@ -73,12 +74,12 @@ object IngestFiles extends LazyLogging with CleanseFiles with ValidateFiles with
     }
   }
 
-  def readFileToDataFrame(spark : SparkSession, filePath : String, envProp : Config) : DataFrame = {
+  def readFileToDataFrame(spark: SparkSession, filePath: String, envProp: Config): DataFrame = {
     val schemaFilePath = envProp.getString("schema.file.path")
     val schema = new StructType()
     spark.read.textFile(schemaFilePath).foreach(r => r.split(":")(0) match {
-      case "int" => schema.add(StructField(r.split(":")(0),IntegerType, true))
-      case _ => schema.add(StructField(r.split(":")(0),StringType, true))
+      case "int" => schema.add(StructField(r.split(":")(0), IntegerType, true))
+      case _ => schema.add(StructField(r.split(":")(0), StringType, true))
     })
     spark.read
       .option("mode", envProp.getString("read.mode"))
